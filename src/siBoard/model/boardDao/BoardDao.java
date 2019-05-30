e0835631e8e2518f7e496e690a35270243759966
roseshade <roseshade90@gmail.com>
2019-05-30 21:11:02 +0900

    검색 후 페이징 게시물번호 rownum 수정(페이지번호 수정필요)

--- src/siBoard/model/boardDao/BoardDao.java
+++ src/siBoard/model/boardDao/BoardDao.java
@@ -16,20 +16,6 @@ import siBoardComment.model.boardCommentVo.BoardComment;
 import siTemplete.JDBCTemplete;
 
 public class BoardDao {
-	private Properties prop = new Properties();
-	public BoardDao() {
-		String fileName = BoardDao.class.getResource("/siSql/board/boardQuery.properties").getPath();
-		System.out.println(fileName);
-		try {
-			prop.load(new FileReader(fileName));
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
 	public int totalCount(Connection conn) {
 		Statement stmt = null;
 		ResultSet rset = null;
@@ -227,18 +213,56 @@ public class BoardDao {
 		}
 		return result;
 	}
-	public ArrayList<Board> boardSearchName(Connection conn,String searchKeyword){
+//	public ArrayList<Board> boardSearchName(Connection conn,String searchKeyword){
+//		ArrayList<Board> list = null;
+//		PreparedStatement pstmt = null;
+//		ResultSet rset = null;
+//		String query = "select * from board where board_id = ?";
+//		try {
+//			pstmt = conn.prepareStatement(query);
+//			pstmt.setString(1, searchKeyword);
+//			rset = pstmt.executeQuery();
+//			list = new ArrayList<Board>();
+//			while(rset.next()) {
+//				Board b = new Board();
+//				b.setBoardNo(rset.getInt("board_no"));
+//				b.setBoardType(rset.getInt("board_Type"));
+//				b.setBoardId(rset.getString("board_id"));
+//				b.setBoardName(rset.getString("board_Name"));
+//				b.setBoardTitle(rset.getString("board_title"));
+//				b.setBoardContent(rset.getString("board_content"));
+//				b.setBoardFilename(rset.getString("board_filename"));
+//				b.setBoardFilepath(rset.getString("board_filepath"));
+//				b.setBoardDate(rset.getDate("board_date"));
+//				b.setBoardCount(rset.getInt("board_count"));
+//				b.setBoardSecret(rset.getInt("board_secret"));
+//				b.setBoardPw(rset.getString("board_pw"));
+//				list.add(b);
+//			}
+//		} catch (SQLException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}finally {
+//			JDBCTemplete.close(rset);
+//			JDBCTemplete.close(pstmt);
+//		}
+//		return list;
+//	}
+	public ArrayList<Board> boardSearchName(Connection conn,String searchKeyword,int start,int end){
 		ArrayList<Board> list = null;
 		PreparedStatement pstmt = null;
 		ResultSet rset = null;
-		String query = "select * from board where board_id = ?";
+		String query = "SELECT * FROM (SELECT ROWNUM AS RNUM, n.* FROM (SELECT * FROM board where board_id = ? ORDER BY BOARD_NO desc) n) WHERE RNUM BETWEEN ? AND ?";
 		try {
 			pstmt = conn.prepareStatement(query);
 			pstmt.setString(1, searchKeyword);
+			pstmt.setInt(2, start);
+			pstmt.setInt(3, end);
 			rset = pstmt.executeQuery();
 			list = new ArrayList<Board>();
 			while(rset.next()) {
 				Board b = new Board();
+				b.setBoardRnum(rset.getInt("rnum"));
 				b.setBoardNo(rset.getInt("board_no"));
 				b.setBoardType(rset.getInt("board_Type"));
 				b.setBoardId(rset.getString("board_id"));
@@ -262,18 +286,21 @@ public class BoardDao {
 		}
 		return list;
 	}
-	public ArrayList<Board> boardSearchTitle(Connection conn,String searchKeyword){
+	public ArrayList<Board> boardSearchTitle(Connection conn,String searchKeyword,int start,int end){
 		ArrayList<Board> list = null;
 		PreparedStatement pstmt = null;
 		ResultSet rset = null;
-		String query = "select * from board where board_Title like ?";
+		String query = "SELECT * FROM (SELECT ROWNUM AS RNUM, n.* FROM (SELECT * FROM board where board_title like ? ORDER BY BOARD_NO desc) n) WHERE RNUM BETWEEN ? AND ?";
 		try {
 			pstmt = conn.prepareStatement(query);
 			pstmt.setString(1, "%"+searchKeyword+"%");
+			pstmt.setInt(2, start);
+			pstmt.setInt(3, end);
 			rset = pstmt.executeQuery();
 			list = new ArrayList<Board>();
 			while(rset.next()) {
 				Board b = new Board();
+				b.setBoardRnum(rset.getInt("rnum"));
 				b.setBoardNo(rset.getInt("board_no"));
 				b.setBoardType(rset.getInt("board_type"));
 				b.setBoardId(rset.getString("board_id"));
@@ -297,6 +324,83 @@ public class BoardDao {
 		}
 		return list;
 	}	
+	public int totalSearchTitleCount(Connection conn,String searchKeyword) {
+		PreparedStatement pstmt = null;
+		ResultSet rset = null;
+		int result = 0;
+		String query = "select count(*) as cnt from board where board_title=?";
+		try {
+			pstmt = conn.prepareStatement(query);
+			pstmt.setString(1, "%"+searchKeyword+"%");
+			rset = pstmt.executeQuery();
+			if(rset.next()) {
+				result = rset.getInt("cnt");
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			JDBCTemplete.close(rset);
+			JDBCTemplete.close(pstmt);
+		}
+		return result;
+	}
+	public int totalSearchNameCount(Connection conn,String searchKeyword) {
+		PreparedStatement pstmt = null;
+		ResultSet rset = null;
+		int result = 0;
+		String query = "select count(*) as cnt from board where board_name=?";
+		try {
+			pstmt = conn.prepareStatement(query);
+			pstmt.setString(1, searchKeyword);
+			rset = pstmt.executeQuery();
+			if(rset.next()) {
+				result = rset.getInt("cnt");
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			JDBCTemplete.close(rset);
+			JDBCTemplete.close(pstmt);
+		}
+		return result;
+	}
+//	public ArrayList<Board> boardSearchTitle(Connection conn,String searchKeyword){
+//		ArrayList<Board> list = null;
+//		PreparedStatement pstmt = null;
+//		ResultSet rset = null;
+//		String query = "select * from board where board_Title like ?";
+//		try {
+//			pstmt = conn.prepareStatement(query);
+//			pstmt.setString(1, "%"+searchKeyword+"%");
+//			rset = pstmt.executeQuery();
+//			list = new ArrayList<Board>();
+//			while(rset.next()) {
+//				Board b = new Board();
+//				b.setBoardNo(rset.getInt("board_no"));
+//				b.setBoardType(rset.getInt("board_type"));
+//				b.setBoardId(rset.getString("board_id"));
+//				b.setBoardName(rset.getString("board_Name"));
+//				b.setBoardTitle(rset.getString("board_title"));
+//				b.setBoardContent(rset.getString("board_content"));
+//				b.setBoardFilename(rset.getString("board_filename"));
+//				b.setBoardFilepath(rset.getString("board_filepath"));
+//				b.setBoardDate(rset.getDate("board_date"));
+//				b.setBoardCount(rset.getInt("board_count"));
+//				b.setBoardSecret(rset.getInt("board_secret"));
+//				b.setBoardPw(rset.getString("board_pw"));
+//				list.add(b);
+//			}
+//		} catch (SQLException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}finally {
+//			JDBCTemplete.close(rset);
+//			JDBCTemplete.close(pstmt);
+//		}
+//		return list;
+//	}	
 	public ArrayList<BoardComment> commentAll(Connection conn){
 		ArrayList<BoardComment> list = null;
 		Statement stmt = null;
--- src/siBoard/model/boardService/BoardService.java
+++ src/siBoard/model/boardService/BoardService.java
@@ -97,15 +97,51 @@ public class BoardService {
 		JDBCTemplete.close(conn);
 		return result;
 	}
-	public ArrayList<Board> boardSearch(String searchType, String searchKeyword){
+	public BoardPageData boardSearch(int reqPage, String searchType, String searchKeyword){
+		//검색 후 페이징 처리가 제대로 안되서 수정 필요
 		Connection conn = JDBCTemplete.getConnection();
+		int numPerPage = 10;
+		int totalCount = new BoardDao().totalCount(conn);
+		int totalPage = (totalCount%numPerPage==0)?(totalCount/numPerPage):(totalCount/numPerPage)+1;
+		int start = (reqPage-1)*numPerPage+1;
+		int end = reqPage*numPerPage;
 		ArrayList<Board> list = new ArrayList<Board>();
 		if(searchType.equals("boardName")) {
-			list = new BoardDao().boardSearchName(conn,searchKeyword);
+			list = new BoardDao().boardSearchName(conn,searchKeyword,start,end);
 		}else if(searchType.equals("boardTitle")) {
-			list = new BoardDao().boardSearchTitle(conn,searchKeyword);
+			list = new BoardDao().boardSearchTitle(conn,searchKeyword,start,end);
+		}
+		String pageNavi = "";
+		int pageNaviSize = 10;
+		int pageNo = ((reqPage-1)/pageNaviSize)*pageNaviSize+1;
+		if(pageNo!=1) {
+			pageNavi+="<a href='/siPreBoard?reqPage="+(pageNo-1)+"'>이전</a>";
+		}
+		int i = 1;
+		while(!(i++>pageNaviSize || pageNo>totalPage)) {
+			if(reqPage==pageNo) {
+				pageNavi+="<span>"+pageNo+"</span>";
+			}else {
+				pageNavi+="<a href='/siPreBoard?reqPage="+pageNo+"'>"+pageNo+"</a>";
+			}
+			pageNo++;
+		}
+		if(pageNo<=totalPage) {
+			pageNavi+="<a href='/siPreBoard?reqPage="+pageNo+"'>다음</a>";
 		}
 		JDBCTemplete.close(conn);
-		return list;
+		BoardPageData bp = new BoardPageData(list,pageNavi);
+		return bp;
 	}
+//	public ArrayList<Board> boardSearch(String searchType, String searchKeyword){
+//		Connection conn = JDBCTemplete.getConnection();
+//		ArrayList<Board> list = new ArrayList<Board>();
+//		if(searchType.equals("boardName")) {
+//			list = new BoardDao().boardSearchName(conn,searchKeyword);
+//		}else if(searchType.equals("boardTitle")) {
+//			list = new BoardDao().boardSearchTitle(conn,searchKeyword);
+//		}
+//		JDBCTemplete.close(conn);
+//		return list;
+//	}
 }
