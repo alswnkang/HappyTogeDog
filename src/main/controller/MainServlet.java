32c4a65958eaef86968cfede0e9166825bc9e29c
alswnkang <tti1080@gmail.com>
2019-06-04 10:46:31 +0900

    메인 봉사활동 리스트 노출 ing

--- src/main/controller/MainServlet.java
+++ src/main/controller/MainServlet.java
@@ -11,6 +11,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import sponsorship.model.vo.ProductVO;
+import volunteer.model.service.VoluntaryService;
+import volunteer.model.vo.VoluntaryRegister;
 
 @WebServlet(name = "Main", urlPatterns = { "/main" })
 public class MainServlet extends HttpServlet {
@@ -22,12 +24,17 @@ public class MainServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		/* 메인 :: 후원하기 상품 노출 */
 		ArrayList<ProductVO> prdList = new ArrayList<ProductVO>();
 		prdList.add(new ProductVO("0", "에코에코에코백", "76896814691427225_1127979769.jpg", "20000"));
 		prdList.add(new ProductVO("1", "배찌뱃지뻇지", "39066105050978558_-1615663619.jpg", "10000"));
 		prdList.add(new ProductVO("2", "달력달력달력", "prd_img01.jpg", "15000"));
 		
 		request.setAttribute("prdList", prdList);
+		
+		/* 메인 :: 봉사활동 게시판 공고 노출 */
+		//ArrayList<VoluntaryRegister> volunList = new VoluntaryService().mainVoluntaryList();
+		
 		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/index.jsp");
 		rd.forward(request, response);
 	}
